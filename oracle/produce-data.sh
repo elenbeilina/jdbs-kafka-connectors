bf8e38dfd3b1200facb0bdd939c49948fa127ceb
beiliele <Elena.Beilina@externals.adidas.com>
2021-05-21 13:16:51 +0300

    added xml transformation

--- oracle/produce-data.sh
+++ oracle/produce-data.sh
@@ -1,9 +1,8 @@
 docker run --tty \
            --network oracle_kafka-connect \
-           -v /Users/aqua-len/IdeaProjects/jdbs-kafka-connectors/oracle/data/test-data-marvel.txt:/test-data-marvel.txt\
+           -v /Users/aqua-len/IdeaProjects/jdbs-kafka-connectors/oracle/data/ASN_20210412164801960.xml:/ASN_20210412164801960.xml\
            confluentinc/cp-kafkacat \
            bash -c "
-            cat /test-data-marvel.txt | kafkacat  \
+            cat /ASN_20210412164801960.xml | kafkacat  \
            -b broker:29092 \
-            -P -t main \
-            -H publisher='Marvel Comics'"
+            -P -t main"
