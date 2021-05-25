65371e1c271beb1c2b04073fe30b69cf8bc49351
beiliele <Elena.Beilina@externals.adidas.com>
2021-05-25 20:17:12 +0300

    - changed test data files - finished xml flattener transformation

--- oracle/produce-data.sh
+++ oracle/produce-data.sh
@@ -1,8 +1,8 @@
 docker run --tty \
            --network oracle_kafka-connect \
-           -v /Users/aqua-len/IdeaProjects/jdbs-kafka-connectors/oracle/data/ASN_20210412164801960.xml:/ASN_20210412164801960.xml\
+           -v /Users/aqua-len/IdeaProjects/jdbs-kafka-connectors/oracle/data/Info.xml:/Info.xml\
            confluentinc/cp-kafkacat \
            bash -c "
-            cat /ASN_20210412164801960.xml | kafkacat  \
+            cat /Info.xml | kafkacat  \
            -b broker:29092 \
             -P -t main"
--- xml-flattener/src/main/java/com/aqualen/xmlflattener/XmlFlattener.java
+++ xml-flattener/src/main/java/com/aqualen/xmlflattener/XmlFlattener.java
@@ -1,23 +1,21 @@
 package com.aqualen.xmlflattener;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.connector.ConnectRecord;
-import org.apache.kafka.connect.data.Date;
-import org.apache.kafka.connect.data.Schema;
-import org.apache.kafka.connect.data.SchemaBuilder;
-import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.data.*;
 import org.apache.kafka.connect.transforms.Transformation;
 
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Optional;
-import java.util.function.Predicate;
-import java.util.logging.Logger;
+import java.util.function.BiPredicate;
 
 public class XmlFlattener<R extends ConnectRecord<R>> implements Transformation<R> {
 
-    private static Logger log = Logger.getLogger(XmlFlattener.class.getName());
-    Predicate<Struct> sizeType = struct -> struct.get("Type").equals("Size");
+    private static final BiPredicate<Struct, Pair<String, String>> structType =
+            (struct, type) -> struct.get(type.getKey()).equals(type.getValue());
 
     @Override
     public ConfigDef config() {
@@ -39,87 +37,99 @@ public class XmlFlattener<R extends ConnectRecord<R>> implements Transformation<
         if (r.value() == null) {
             return r;
         }
-        Schema asnSchema = SchemaBuilder.struct().name("ASN")
+        Schema asnSchema = SchemaBuilder.struct().name("Info")
                 .field("ASNMessageID", Schema.OPTIONAL_STRING_SCHEMA)
                 .field("EstDepartDate", Date.SCHEMA)
                 .field("EstDischargePortDate", Date.SCHEMA)
-                .field("Mode", Schema.STRING_SCHEMA)
-                .field("ShipmentID", Schema.STRING_SCHEMA)
-                .field("ShipmentStatus", Schema.STRING_SCHEMA)
-                .field("Vessel", Schema.STRING_SCHEMA)
-                .field("Voyage", Schema.STRING_SCHEMA)
-                .field("OriginCityName", Schema.STRING_SCHEMA)
-                .field("OriginCountryCode", Schema.STRING_SCHEMA)
-                .field("PartyInfoName", Schema.STRING_SCHEMA)
-                .field("ContainerType", Schema.STRING_SCHEMA)
-                .field("ContainerLoad", Schema.STRING_SCHEMA)
-                .field("ContainerNumber", Schema.STRING_SCHEMA)
-                .field("ContainerTypeName", Schema.STRING_SCHEMA)
-                .field("LoadPlanName", Schema.STRING_SCHEMA)
-                .field("ReferenceNumber", Schema.STRING_SCHEMA)
-                .field("SealNumber", Schema.STRING_SCHEMA)
-                .field("ServiceType", Schema.STRING_SCHEMA)
-                .field("BLNumber", Schema.STRING_SCHEMA)
-                .field("Division", Schema.STRING_SCHEMA)
-                .field("InvoiceNumber", Schema.STRING_SCHEMA)
-                .field("LineItemNumber", Schema.STRING_SCHEMA)
-                .field("LoadSequence", Schema.STRING_SCHEMA)
-                .field("ManufacturerID", Schema.STRING_SCHEMA)
-                .field("ShippeCartoons", Schema.STRING_SCHEMA)
-                .field("PONumber", Schema.STRING_SCHEMA)
-                .field("ProductCode", Schema.STRING_SCHEMA)
-                .field("ProductName", Schema.STRING_SCHEMA)
-                .field("ReferenceNumber1", Schema.STRING_SCHEMA)
-                .field("ReferenceNumber2", Schema.STRING_SCHEMA)
-                .field("FactoryName", Schema.STRING_SCHEMA)
-                .field("WeightUnit", Schema.STRING_SCHEMA)
-                .field("Weight", Schema.INT16_SCHEMA)
-                .field("TotalWeightUnit", Schema.STRING_SCHEMA)
-                .field("TotalWeight", Schema.INT16_SCHEMA)
-                .field("SizeIndex", Schema.STRING_SCHEMA)
-                .field("UPC", Schema.STRING_SCHEMA)
-                .field("SKU", Schema.STRING_SCHEMA)
-                .field("CartoonCount", Schema.STRING_SCHEMA)
-                .field("MCCFlag_LegIndicator", Schema.STRING_SCHEMA)
-                .field("OrderRef3", Schema.STRING_SCHEMA)
-                .field("Milestone - Actual time of arrival (ATA)", Schema.STRING_SCHEMA)
-                .field("Milestone - Transport ATD Origin", Schema.STRING_SCHEMA)
-                .field("Milestone - Transport ETA Origin", Schema.STRING_SCHEMA)
-                .field("Gate in Origin", Schema.STRING_SCHEMA)
-                .field("Port of LoadingCountryCode", Schema.STRING_SCHEMA)
-                .field("Port of LoadingCity", Schema.STRING_SCHEMA)
-                .field("PortofDischarge-CityName", Schema.STRING_SCHEMA)
-                .field("PortofDischarge-CountryCode", Schema.STRING_SCHEMA)
-                .field("Received (Pickup, CFS or CY) - FCR date", Schema.STRING_SCHEMA)
-                .field("Shipped Volume", Schema.STRING_SCHEMA)
-                .field("VolumeUnit", Schema.STRING_SCHEMA)
-                .field("Shipped Qty", Schema.STRING_SCHEMA)
-                .field("Shipped Weight", Schema.STRING_SCHEMA)
-                .field("Shipped Weight Unit", Schema.STRING_SCHEMA)
-                .field("Transport ETD Origin", Schema.STRING_SCHEMA)
+                .field("Mode", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("ShipmentID", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("ShipmentStatus", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("Vessel", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("Voyage", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("OriginCityName", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("OriginCountryCode", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("PartyInfoName", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("ContainerType", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("ContainerLoad", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("ContainerNumber", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("ContainerTypeName", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("LoadPlanName", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("ReferenceNumber", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("SealNumber", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("ServiceType", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("BLNumber", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("Division", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("InvoiceNumber", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("LineItemNumber", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("LoadSequence", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("ManufacturerID", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("ShippeCartoons", Schema.OPTIONAL_INT64_SCHEMA)
+                .field("PONumber", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("ProductCode", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("ProductName", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("ReferenceNumber1", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("ReferenceNumber2", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("FactoryName", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("WeightUnit", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("Weight", Decimal.schema(0))
+                .field("TotalWeightUnit", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("TotalWeight", Decimal.schema(0))
+                .field("SizeIndex", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("UPC", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("SKU", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("CartoonCount", Schema.OPTIONAL_INT64_SCHEMA)
+                .field("MCCFlag_LegIndicator", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("OrderRef3", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("Milestone-Transport_ATD_Origin", Date.builder().optional().schema())
+                .field("Milestone-Transport_ETA_Origin", Date.builder().optional().schema())
+                .field("Port_of_LoadingCountryCode", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("Port_of_LoadingCity", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("PortofDischarge-CityName", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("PortofDischarge-CountryCode", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("Shipped_Volume", Decimal.builder(0).optional().schema())
+                .field("VolumeUnit", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("Shipped_Qty", Schema.OPTIONAL_FLOAT64_SCHEMA)
+                .field("Shipped_Weight", Decimal.builder(0).optional().schema())
+                .field("Shipped_Weight Unit", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("Transport_ETD_Origin", Date.builder().optional().schema())
                 .build();
 
         Struct transformedValue = (Struct) r.value();
-        ArrayList<Struct> asnList = (ArrayList<Struct>) transformedValue.get("ASN");
-        Struct asn = asnList.get(0) ;
+        Struct asn = extractStructFromArray(transformedValue, "ASN");
 
         Struct originCity = asn.getStruct("OriginCity");
-        Struct asnPartyInfo = asn.getStruct("PartyInfo");
-        Struct container = asn.getStruct("Container");
+        Struct partyInfo = extractStructFromArray(asn, "PartyInfo");
 
-        ArrayList<Struct> lineItems = (ArrayList<Struct>)container.get("LineItems");
-        Struct lineItem = lineItems.get(0);
-        Struct lineItemPartyInfo = lineItem.getStruct("PartyInfo");
+        Struct container = extractStructFromArray(asn, "Container");
+
+        Struct lineItem = extractStructFromArray(container, "LineItems");
+        Struct lineItemPartyInfo = extractStructFromArray(lineItem, "PartyInfo");
         Struct weight = lineItem.getStruct("Weight");
 
-        Struct assortment = lineItem.getStruct("Assortment");
+        Struct assortment = extractStructFromArray(lineItem, "Assortment");
         Struct totalWeight = assortment.getStruct("TotalWeight");
-        ArrayList<Struct> references = (ArrayList<Struct>) assortment.get("References");
-        Struct sizeIndex = references.stream().filter(sizeType).findFirst().get();
 
+        Struct assortmentSize = extractStructFromArray(assortment, "References",
+                new ImmutablePair<>("Type", "Size"));
+        Struct lineItemBlRefNum1 = extractStructFromArray(lineItem, "References",
+                new ImmutablePair<>("Type", "BlRefNum1"));
+        Struct lineItemOrderRef3 = extractStructFromArray(lineItem, "References",
+                new ImmutablePair<>("Type", "OrderRef3"));
+
+        Struct referenceDateATDOrigin = extractStructFromArray(asn, "ReferenceDates",
+                new ImmutablePair<>("ReferenceDateType", "Transport ATD Origin"));
+        Struct referenceDateETADestination = extractStructFromArray(asn, "ReferenceDates",
+                new ImmutablePair<>("ReferenceDateType", "Transport ETA Destination"));
+        Struct referenceDateETDOrigin = extractStructFromArray(asn, "ReferenceDates",
+                new ImmutablePair<>("ReferenceDateType", "Transport ETD Origin"));
+
+        Struct portOfLoading = asn.getStruct("PortOfLoading");
+        Struct portOfDischarge = asn.getStruct("PortOfDischarge");
+        Struct volume = container.getStruct("Volume");
+        Struct actualWight = container.getStruct("ActualWeight");
 
         Struct asnStruct = new Struct(asnSchema)
-                .put("ASNMessageID", Optional.ofNullable(asn.get("ASNMessageID")).orElse(null))
+                .put("ASNMessageID", getFieldWIthNullCheck(asn, "ASNMessageID"))
                 .put("EstDepartDate", asn.get("EstDepartDate"))
                 .put("EstDischargePortDate", asn.get("EstDischargePortDate"))
                 .put("Mode", asn.get("Mode"))
@@ -129,7 +139,7 @@ public class XmlFlattener<R extends ConnectRecord<R>> implements Transformation<
                 .put("Voyage", asn.get("Voyage"))
                 .put("OriginCityName", originCity.get("CityName"))
                 .put("OriginCountryCode", originCity.get("CountryCode"))
-                .put("PartyInfoName", asnPartyInfo.get("Name"))
+                .put("PartyInfoName", partyInfo.get("Name"))
                 .put("ContainerType", container.get("ContainerType"))
                 .put("ContainerLoad", container.get("ContainerLoad"))
                 .put("ContainerNumber", container.get("ContainerNumber"))
@@ -150,33 +160,33 @@ public class XmlFlattener<R extends ConnectRecord<R>> implements Transformation<
                 .put("ProductName", lineItem.get("ProductName"))
                 .put("ReferenceNumber1", lineItem.get("ReferenceNumber1"))
                 .put("ReferenceNumber2", lineItem.get("ReferenceNumber2"))
-                .put("FactoryName", lineItemPartyInfo.get("Name"))
+                .put("FactoryName", getFieldWIthNullCheck(lineItemPartyInfo, "Name"))
                 .put("WeightUnit", weight.get("ANSICode"))
                 .put("Weight", weight.get("value"))
                 .put("TotalWeightUnit", totalWeight.get("ANSICode"))
                 .put("TotalWeight", totalWeight.get("value"))
-                .put("SizeIndex", sizeIndex.get("Value"))
+                .put("SizeIndex", getFieldWIthNullCheck(assortmentSize, "Value"))
                 .put("UPC", assortment.get("UPC"))
-                .put("SKU", assortment.get("SKU"))
-                .put("CartoonCount", assortment.get("CartoonCount"))
-//                .put("MCCFlag_LegIndicator", asn.get("EstDischargePortDate"))
-//                .put("OrderRef3", asn.get("EstDischargePortDate"))
-//                .put("Milestone - Actual time of arrival (ATA)", asn.get("EstDischargePortDate"))
-//                .put("Milestone - Transport ATD Origin", asn.get("EstDischargePortDate"))
-//                .put("Milestone - Transport ETA Origin", asn.get("EstDischargePortDate"))
-//                .put("Gate in Origin", asn.get("EstDischargePortDate"))
-//                .put("Port of LoadingCountryCode", asn.get("EstDischargePortDate"))
-//                .put("Port of LoadingCity", asn.get("EstDischargePortDate"))
-//                .put("PortofDischarge-CityName", asn.get("EstDischargePortDate"))
-//                .put("PortofDischarge-CountryCode", asn.get("EstDischargePortDate"))
-//                .put("Received (Pickup, CFS or CY) - FCR date", asn.get("EstDischargePortDate"))
-//                .put("Shipped Volume", asn.get("EstDischargePortDate"))
-//                .put("VolumeUnit", asn.get("EstDischargePortDate"))
-//                .put("Shipped Qty", asn.get("EstDischargePortDate"))
-//                .put("Shipped Weight", asn.get("EstDischargePortDate"))
-//                .put("Shipped Weight Unit", asn.get("EstDischargePortDate"))
-//                .put("Transport ETD Origin", asn.get("EstDischargePortDate"))
-        ;
+                .put("SKU", assortment.get("Sku"))
+                .put("CartoonCount", assortment.get("CartonCount"))
+                .put("MCCFlag_LegIndicator", getFieldWIthNullCheck(lineItemBlRefNum1, "Value"))
+                .put("OrderRef3", getFieldWIthNullCheck(lineItemOrderRef3, "Value"))
+                .put("Milestone-Transport_ATD_Origin", getFieldWIthNullCheck(
+                        referenceDateATDOrigin, "ReferenceDate")
+                )
+                .put("Milestone-Transport_ETA_Origin", getFieldWIthNullCheck(
+                        referenceDateETADestination, "ReferenceDate")
+                )
+                .put("Port_of_LoadingCountryCode", portOfLoading.get("CountryCode"))
+                .put("Port_of_LoadingCity", portOfLoading.get("CityName"))
+                .put("PortofDischarge-CityName", portOfDischarge.get("CityName"))
+                .put("PortofDischarge-CountryCode", portOfDischarge.get("CountryCode"))
+                .put("Shipped_Volume", volume.get("value"))
+                .put("VolumeUnit", volume.get("Unit"))
+                .put("Shipped_Qty", getFieldWIthNullCheck(lineItem.getStruct("Quantity"), "value"))
+                .put("Shipped_Weight", actualWight.get("value"))
+                .put("Shipped_Weight Unit", actualWight.get("Unit"))
+                .put("Transport_ETD_Origin", getFieldWIthNullCheck(referenceDateETDOrigin, "ReferenceDate"));
         try {
             return r.newRecord(
                     r.topic(),
@@ -190,5 +200,30 @@ public class XmlFlattener<R extends ConnectRecord<R>> implements Transformation<
         } catch (Exception e) {
             throw new MappingException(String.format("Unable to process message: %s", e.getMessage()), e);
         }
+    }
+
+    private Struct extractStructFromArray(Struct parent, String childName) {
+        ArrayList<Struct> child = (ArrayList<Struct>) parent.get(childName);
+
+        return Optional
+                .ofNullable(child)
+                .map(structs -> structs.get(0))
+                .orElse(null);
+    }
+
+    private Struct extractStructFromArray(Struct parent, String childName, Pair<String, String> type) {
+        ArrayList<Struct> child = (ArrayList<Struct>) parent.get(childName);
+
+        Optional<Struct> structOptional = child
+                .stream()
+                .filter(struct -> structType.test(struct, type))
+                .findFirst();
+        return structOptional.orElse(null);
+    }
+
+    private Object getFieldWIthNullCheck(Struct struct, String fieldName) {
+        return Optional.ofNullable(struct)
+                .map(value -> value.get(fieldName))
+                .orElse(null);
     }
 }
